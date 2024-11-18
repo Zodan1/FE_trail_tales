92e021c51099883fae30e488383b6e9750ffdead
Zodan1 <dannyz93@msn.com>
2024-11-18 17:10:52 +0000

    Created UI for add screen

--- app/(tabs)/add.tsx
+++ app/(tabs)/add.tsx
@@ -1,9 +1,20 @@
-import { Text, View, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { Image } from "expo-image";
+import ImageViewer from "@/components/ImageViewer";
+import Button from "@/components/Button";
+
+const PlaceholderImage = require("../../assets/images/tiger.jpg");
 
 export default function AddScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Add screen</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
     </View>
   );
 }
@@ -12,10 +23,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#25292e",
-    justifyContent: "center",
     alignItems: "center",
   },
-  text: {
-    color: "#fff",
+  imageContainer: {
+    flex: 1,
+    paddingTop: 28,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
